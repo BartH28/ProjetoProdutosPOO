b19c9a6a405993c6b958ebb050d06164c873dcc8
Henrique Barreto <henriquelb28@gmail.com>
2021-06-07 16:22:24 -0300

    estrutura de inteface e classe feita sem teste

--- intefaces/Imarca.cs
+++ intefaces/Imarca.cs
@@ -1,7 +1,12 @@
+using Projeto_Produtos.Classes;
+
 namespace Projeto_Produtos.intefaces
 {
     public interface Imarca
     {
-        
+        string Cadastrar(Marca marca);
+        void Listar();
+
+        string Deletar(Marca marca);
     }
 }
